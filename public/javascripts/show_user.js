dda43232f10ff545f00ed1b048b9939bb3f8a7a3
TJWilson92 <wislong@hotmail.co.uk>
2015-07-31 22:35:40 +0100

    Show user page sorted

--- public/javascripts/show_user.js
+++ public/javascripts/show_user.js
@@ -3,22 +3,31 @@ var account_id = window.location.href.split('/').reverse()[0].toString();
 var toggleDemonstrator = function() {
   $.ajax({
     url: '../../users/toggleDemonstrator',
-    method: 'get',
+    method: 'post',
     type: 'json',
     data: {account_id: account_id}
   }).done(function(){
-    Location.reload();
+    document.location.reload();
   })
 }
 
 // AJAX PUT to /users/toggleAdmin - parse account_id from URL
-var toggleDemonstrator = function() {
+var toggleAdmin = function() {
   $.ajax({
     url: '../../users/toggleAdmin',
-    method: 'get',
+    method: 'post',
     type: 'json',
     data: {account_id: account_id}
   }).done(function(){
-    Location.reload();
+    document.location.reload();
   })
 }
+
+$(document).ready(function(){
+  $('#toggleDemonstratorButton').click(function(){
+    toggleDemonstrator();
+  });
+  $('#toggleAdminButton').click(function(){
+    toggleAdmin();
+  })
+})
--- routes/users.js
+++ routes/users.js
@@ -25,23 +25,27 @@ router.get('/show/:id', function(req, res, next){
 });
 
 router.post('/toggleDemonstrator', function(req, res, next){
-  Account.findById(req.query.account_id, function(err, account){
+  Account.findById(req.body.account_id, function(err, account){
     if (account.isDemonstrator) {
       account.isDemonstrator = false;
+      account.save();
     } else {
       account.isDemonstrator = true;
+      account.save();
     }
-    account.save();
+
     res.send('Changed');
   });
 });
 
 router.post('/toggleAdmin', function(req, res, next){
-  Account.findById(req.query.account_id, function(err, account){
+  Account.findById(req.body.account_id, function(err, account){
     if (account.isAdmin) {
       account.isAdmin = false;
+      account.save();
     } else {
       account.isAdmin = true;
+      account.save();
     }
     account.save();
     res.send('Changed');
